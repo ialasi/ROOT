85f888696badc70917e19845659a859aa0ace04e
tayo Alasi <ibitayoalasi@gmail.com>
2026-02-16 15:36:02 +0100

    Add variables exercise

---
+++ ROOT/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare variables
+	var name string = "tayo Alasi"
+	var age int = 37
+	var height float64 = 1.81
+
+	// Print variables
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(height)
+
+	// Bonus: short declaration
+	city := "Abuja"
+	fmt.Println(city)
+}
