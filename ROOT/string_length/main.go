a8fd68e40fb14b13a9e2bcc0cc36b85db5a69a87
tayo Alasi <ibitayoalasi@gmail.com>
2026-02-17 14:25:05 +0100

    string_length

--- ROOT/string_length/main.go
+++ ROOT/string_length/main.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func main()
-// Declare a string
-text := "Hello, Go!" 
+func main() {
+	// Declare a string
+	text := "Hello, Go!"
 
-// Print the string
-fmt.Println("Text:", text)
+	// Print the string
+	fmt.Println("Text:", text)
 
-// Print the length of the string
-fmt.Println("Length:", len(text))
+	// Print the length of the string
+	fmt.Println("Length:", len(text))
 }
