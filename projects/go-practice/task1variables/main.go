24e13196103700a6181374e16bdb03fabd1ab0e5
tayo Alasi <ibitayoalasi@gmail.com>
2026-04-27 16:01:09 +0100

    task1variables

---
+++ projects/go-practice/task1variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Ibitayo"
+	age := 37
+	isStudent := true
+
+	fmt.Println("my name is", name)
+	fmt.Println("and i am", age, "years old")
+	fmt.Println("Student status", isStudent)
+}
