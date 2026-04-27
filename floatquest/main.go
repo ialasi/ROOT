221cf3817752ba0b774b8a231dfb5823c6b9407c
tayo Alasi <ibitayoalasi@gmail.com>
2026-04-27 14:35:39 +0100

    floatquest

---
+++ floatquest/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	pi := 3.149
+	temperature := 72.5
+	height := 5.9
+
+	fmt.Println("Pi approximately equals", pi)
+	fmt.Println("Temperature", temperature)
+	fmt.Println("Height", height)
+}
