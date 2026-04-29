2e502950201eb4211c83bbbd0a28fcf5eb44df50
tayo Alasi <ibitayoalasi@gmail.com>
2026-04-29 12:04:21 +0100

    task4

---
+++ projects/task4/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 0
+
+	if number > 0 {
+		fmt.Println("Positive") 
+	}
+	else if numner < 0 {
+		fmt.Println("Negative") 
+	} else {
+		fmtPrintln("Zero")
+	}
+}
