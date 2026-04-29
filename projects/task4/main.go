e82db6e53e2e38d4088cab32dcd1efe3c5c8c9a6
tayo Alasi <ibitayoalasi@gmail.com>
2026-04-29 12:09:41 +0100

    Add task4: check positive, negative, or zero

--- projects/task4/main.go
+++ projects/task4/main.go
@@ -6,11 +6,10 @@ func main() {
 	number := 0
 
 	if number > 0 {
-		fmt.Println("Positive") 
-	}
-	else if numner < 0 {
-		fmt.Println("Negative") 
+		fmt.Println("Positive")
+	} else if number < 0 {
+		fmt.Println("Negative")
 	} else {
-		fmtPrintln("Zero")
+		fmt.Println("Zero")
 	}
 }
