55ff6757496db60f70fd8a58ffcacb5714a9d726
tayo Alasi <ibitayoalasi@gmail.com>
2026-04-29 11:23:02 +0100

    task3

---
+++ projects/task3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 100
+	b := 20
+
+	sum := a + b
+	product := a * b
+	difference := a - b
+	division := float64(a) / float64(b)
+
+	fmt.Println("Sum", sum)
+	fmt.Println("Product", product)
+	fmt.Println("Difference", difference)
+	fmt.Println("Division", division)
+}
